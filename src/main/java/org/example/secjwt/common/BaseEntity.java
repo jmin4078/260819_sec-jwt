1e877e9689c436204027e09c0bc92c6432ab6b68
jmin4078 <wpalsrl8588@gmail.com>
2026-08-19 10:26:10 +0900

    Fix Swagger security route

--- src/main/java/org/example/secjwt/common/BaseEntity.java
+++ src/main/java/org/example/secjwt/common/BaseEntity.java
@@ -1,8 +1,8 @@
 package org.example.secjwt.common;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
